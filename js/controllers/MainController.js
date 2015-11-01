88f07d50ef95e48f1188b42fa1551bd151ee1ffc
chuah48263 <chuah48263@gmail.com>
2015-11-01 20:10:32 +0800

    http.get json

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,1 +1,8 @@
-app.controller('MainController', ['$scope', '$http', function($scope, $http) {}]);
+'use strict';
+
+app.controller('MainController', ['$scope', '$http', function($scope, $http) {
+  $http.get('http://www.freecodecamp.com/news/hot?callback=JSON_CALLBACK')
+  .success(function(data){
+    console.log(data);
+  });
+}]);
