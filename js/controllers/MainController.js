f6beadf211a5b5ea81b49762b1441ab24af21f29
chuah48263 <chuah48263@gmail.com>
2015-11-01 20:15:01 +0800

    http.get json

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,8 +1,9 @@
 'use strict';
 
 app.controller('MainController', ['$scope', '$http', function($scope, $http) {
+  var stories;
   $http.get('http://www.freecodecamp.com/news/hot?callback=JSON_CALLBACK')
   .success(function(data){
-    console.log(data);
+    stories = data;
   });
 }]);
